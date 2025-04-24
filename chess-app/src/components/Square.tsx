c53a8c67ed13451d27eaf547e9052694e952304b
vinayakgupta710 <guptavinayak710@gmail.com>
2025-04-25 01:26:15 +0930

    added rank and file number

--- chess-app/src/components/Square.tsx
+++ chess-app/src/components/Square.tsx
@@ -1,11 +1,50 @@
+import { useState } from "react";
+
 interface SquareProps {
     idx: number;
 }
 
 export default function Square({ idx }: SquareProps) {
+    const rank = 8 - Math.floor(idx / 8);
+    const file = String.fromCharCode(97 + (idx % 8));
+
     const isDark = ((Math.floor(idx / 8) + idx) % 2) === 1;
-    
+
+    const [squareColour, setSquareColour] = useState(isDark ? '#739552' : '#EBECD0');
+    const [textColour, setTextColour] = useState(!isDark ? '#739552' : '#EBECD0');
+    const changeColour = () => {
+        if (squareColour === '#D46C51' || squareColour === '#ED7E6A') {
+            setSquareColour(isDark ? '#739552' : '#EBECD0');
+            setTextColour(!isDark ? '#739552' : '#EBECD0');
+        } else {
+            setSquareColour(isDark ? '#D46C51' : '#ED7E6A');
+            setTextColour(!isDark ? '#D46C51' : '#ED7E6A');
+        }
+    }
+
     return (
-        <div className={`aspect-square ${isDark ? 'bg-[#739552]' : 'bg-[#EBECD0]'}`} /> 
+        <div
+            className="aspect-square relative"
+            style={{ background: `${squareColour}` }}
+            onClick={() => changeColour()}
+        >
+            {idx % 8 == 0 && (
+                <p 
+                    className="text-md text-left font-medium p-1"
+                    style={{ color: `${textColour}` }}
+                >
+                    {rank}
+                </p>
+            )}
+
+            {idx >= 56 && (
+                <p 
+                    className="absolute bottom-0 right-0 text-md text-right font-medium p-1"
+                    style={{ color: `${textColour}` }}     
+                >
+                    {file}
+                </p>
+            )}
+        </div>
     );
 };
